69a02bc071b3d9747f2c8d33ab2c8e83dade25e2
raulfm94 <raulfm94@gmail.com>
2018-03-05 18:39:33 -0600

    Restaurando el branch

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+
+public class CubeScript : MonoBehaviour {
+
+	public void OnGazeEnter(){
+		Debug.Log ("Gaze entered");
+	}
+
+	public void OnGazeLeave(){
+		Debug.Log ("Gaze left");
+	}
+
+	public void Fire(){
+		//transform.position += new Vector3 (2, 0, 0);
+		print("Holi");
+	}
+}
