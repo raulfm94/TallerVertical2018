45986bb5fa3493c8429a1767d4fe0ec8fee1c750
raulfm94 <raulfm94@gmail.com>
2018-03-07 12:14:15 -0600

    Todos los features en uno solo

---
+++ Assets/NewAssets/Isma-Textos/ButtonOK.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonOK : MonoBehaviour {
+
+	private Animator am;
+	public bool activate;
+
+	// Use this for initialization
+	void Start () {
+		am = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		am.SetBool ("Activate", activate);
+	}
+
+	public void StartCoroutine(){
+		activate = true;
+		StartCoroutine ("DestroyButton");
+	}
+
+	// AQUI OCURRE LA INTERACCIÓN CON EL MENU, LA DESTRUCCIÓN DEL MISMO Y EL CAMBIO DE ESCENA. 
+	IEnumerator DestroyButton(){
+		while(true){
+			yield return new WaitForSeconds(5);
+			Destroy(this.gameObject);
+			SceneManager.LoadScene("Scene1");
+
+			
+		}
+	}
+
+	public void FalseActivate(){
+		activate = false;
+	}
+}
