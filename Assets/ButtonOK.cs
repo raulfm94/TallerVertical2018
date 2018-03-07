b8be430e3c4c312520a2230e2f740e4903aef148
IsmaLga <a01630333@itesm.mx>
2018-03-06 20:45:02 -0600

    ui v1
    Signed-off-by: IsmaLga <a01630333@itesm.mx>

---
+++ Assets/ButtonOK.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonOK : MonoBehaviour {
+
+	private Animator am;
+	public bool activate;
+
+	// Use this for initialization
+	void Start () {
+		am = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		am.SetBool ("Activate", activate);
+	}
+
+	public void StartCoroutine(){
+		activate = true;
+		StartCoroutine ("DestroyButton");
+	}
+
+	IEnumerator DestroyButton(){
+		while(true){
+			yield return new WaitForSeconds(5);
+			Destroy(this.gameObject);
+			
+		}
+	}
+
+	public void FalseActivate(){
+		activate = false;
+	}
+}
---
+++ Assets/GoogleVR/Scripts/BTN.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTN : MonoBehaviour {
+
+	private Camera cam; 
+	private Transform cameraTransform;
+
+	// Use this for initialization
+	void Start () {
+		cameraTransform = GameObject.Find("Main Camera").transform;
+		cam = cameraTransform.GetComponent<Camera> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit, 10000)){
+				Debug.DrawLine(ray.origin, ray.direction);
+				Debug.Log("Clicked on" + hit.transform.gameObject.name);
+				Destroy(hit.transform.gameObject);
+			}
+				
+		}
+	}
+}
