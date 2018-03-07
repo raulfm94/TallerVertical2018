64e45d228ed14904db5a7fe79598012336c39e41
IsmaLga <a01630333@itesm.mx>
2018-03-06 21:03:24 -0600

    ui v1.1
    Signed-off-by: IsmaLga <a01630333@itesm.mx>

--- Assets/ButtonOK.cs
+++ Assets/ButtonOK.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class ButtonOK : MonoBehaviour {
 
@@ -22,10 +24,13 @@ public class ButtonOK : MonoBehaviour {
 		StartCoroutine ("DestroyButton");
 	}
 
+	// AQUI OCURRE LA INTERACCIÓN CON EL MENU, LA DESTRUCCIÓN DEL MISMO Y EL CAMBIO DE ESCENA. 
 	IEnumerator DestroyButton(){
 		while(true){
 			yield return new WaitForSeconds(5);
 			Destroy(this.gameObject);
+			SceneManager.LoadScene("Scene1");
+
 			
 		}
 	}
